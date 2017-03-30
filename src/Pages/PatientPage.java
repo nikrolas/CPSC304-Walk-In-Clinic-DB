39e4ac72fce4b4bb4a71ee283588f1313792d5af
Hayley Milne <hayley.e.miln@gmail.com>
2017-03-29 22:22:56 -0700

    Added add patient and delete patient

--- src/Pages/PatientPage.java
+++ src/Pages/PatientPage.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Hayley on 2017-03-28.
@@ -92,14 +93,14 @@ public class PatientPage {
             ps.setString(1,oldFirstName);
             ps.setString(2,oldLastName);
             rs = ps.executeQuery();
-             patientID = rs.getInt("PatientID");
+            patientID = rs.getInt("PatientID");
             ps.close();
 
             ps = con.prepareStatement("update Patients set FirstName = ?,  LastName = ? where PatientID = ?");
             ps.setString(1,newFirstName);
             ps.setString(2,newLastName);
             ps.setInt(3,patientID);
-            rs = ps.executeQuery();
+            ps.executeUpdate();
             ps.close();
 
             ps = con.prepareStatement("update Contacts set aptHouseNumber = ?, Street = ?, City = ?, PostalCode = ?, Province = ?, PhoneNumber = ?, Notes = ?  where FK_PatientID = ?");
@@ -111,7 +112,7 @@ public class PatientPage {
             ps.setInt(6, (Integer) phoneNumber);
             ps.setString(7, notes);
             ps.setInt(8, patientID);
-            rs = ps.executeQuery();
+            ps.executeUpdate();
             ps.close();
         }
         catch(SQLException ex)
@@ -121,13 +122,88 @@ public class PatientPage {
     }
 
     //Adds patient + Contact Info to database
-    public void addPatient(){
+    public void addPatient(String firstName, String lastName, String gender, String InsuranceProviderName, int PatientPrescriptionID,  Number aptHouseNumber,String street, String city, String postalCode, String province, Number phoneNumber, String notes){
+        PreparedStatement ps;
+        ResultSet rs;
 
+        try {
+            ps = con.prepareStatement("SELECT * FROM InsuranceProviders  WHERE " + "InsuranceProviderName = ?");
+            ps.setString(1, InsuranceProviderName);
+            rs = ps.executeQuery();
+            int insuranceProviderID = rs.getInt("InsuranceProviderID");
+            ps.close();
+
+
+            Random random = new Random();
+            int patientID = random.nextInt();
+            String SQL = "INSERT INTO Patients VALUES (?,?,?,?,?,?)";
+
+            ps = con.prepareStatement(SQL);
+            ps.setInt(1, patientID);
+            ps.setString(2, firstName);
+            ps.setString(3, lastName);
+            ps.setString(4, gender);
+            ps.setInt(5, insuranceProviderID);
+            ps.setInt(6, PatientPrescriptionID);
+
+            ps.executeUpdate();
+            ps.close();
+
+             random = new Random();
+            int contactID = random.nextInt();
+            SQL = "INSERT INTO Contacts VALUES (?,?,?,?,?,?,?,?,?)";
+            ps = con.prepareStatement(SQL);
+
+            ps.setInt(1, contactID);
+            ps.setInt(2,patientID);
+            ps.setInt(3, (Integer) aptHouseNumber);
+            ps.setString(4, street);
+            ps.setString(5, city);
+            ps.setString(6, postalCode);
+            ps.setString(7, province);
+            ps.setInt(8, (Integer) phoneNumber);
+            ps.setString(9,  notes);
+            ps.executeUpdate();
+            ps.close();
+        }
+        catch (SQLException ex){
+            System.out.println("Message: "+ex.getMessage());
+
+        }
     }
 
     //Delete patient + Contact Info to database
-    public void deletePatient(){
+    public void deletePatient(String firstName, String lastName){
+        PreparedStatement ps;
+        ResultSet rs;
 
+        try {
+            ps = con.prepareStatement("SELECT * FROM PATIENTS WHERE " + "FirstName= ? AND LastName = ?");
+            ps.setString(1,firstName);
+            ps.setString(2,lastName);
+            rs = ps.executeQuery();
+            int patientID = rs.getInt("PatientID");
+            ps.close();
+
+            String sql = "DELETE FROM Patients  WHERE " + "FirstName = ? AND LastName = ?" ;
+            ps = con.prepareStatement(sql);
+
+            ps.setString(1,  firstName);
+            ps.setString(2, lastName );
+            ps.executeUpdate();
+            ps.close();
+
+            sql = "DELETE FROM Contacts WHERE " + "FK_PatientID = ?" ;
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1,  patientID);
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch(SQLException ex){
+            System.out.println("Message: "+ex.getMessage());
+        }
     }
 
 
