c606ba23b28f9eda88bb09a09d217a1eec0c3d23
Hayley Milne <hayley.e.miln@gmail.com>
2017-03-31 12:57:25 -0700

    Small Changes

--- src/Pages/AppointmentPage.java
+++ src/Pages/AppointmentPage.java
@@ -10,8 +10,32 @@ import java.util.Random;
 /**
  * Created by Hayley on 2017-03-28.
  */
+/*
+SELECT cname
+        FROM Customer C
+        WHERE NOT EXISTS
+        ((SELECT I.iid
+        FROM Item I)
+        EXCEPT
+        (SELECT R.iid
+        FROM Order R
+        WHERE R.cid=C.cid))
+
+Select PatientID FROM Patients P WHERE  EXISTS (SELECT A.AppointmentID FROM Appointments A) EXCEPT
+SELECT PhoneNumber FROM Contact  WHERE PatientID = FK_PatientID;
+
+Select PatientID FROM Patients P Where EXISTS ((SELECT AppointmentID from Appointments A where A.FK_PatientID = P.PatientID) Minus (SELECT FK_PatientID FROM Contacts));
+
+Select PatientID FROM Patients P Where EXISTS ((SELECT AppointmentID from Appointments A where A.FK_PatientID = P.PatientID) Minus (SELECT c.PhoneNumber FROM Contacts c WHERE PatientID = c.FK_PatientID AND ))
+
+Select PatientID FROM Patients P Where Not EXISTS ((SELECT AppointmentID from Appointments A where A.FK_PatientID = P.PatientID) Minus (SELECT c.PhoneNumber FROM Contacts c WHERE PatientID = c.FK_PatientID)) // Add a not exists
+
+        */
+
+
 public class AppointmentPage {
     private Connection con;
+    private int userID;
 
     // Get Appointments by Time, Date and Patient Name
     public ArrayList<Appointment> getAppointmentsbyDoctor(Date appointmentDate, Number appointmentTime, int patientID){
